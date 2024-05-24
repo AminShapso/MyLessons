751ead3bec97a6c8b5c697e3f24c63b4f43e761b
Amin-Sha <157518686+Amin-Sha@users.noreply.github.com>
2024-05-24 16:02:01 +0300

    fixed typo

--- Lessons 02 (2024)/Lesson 03 - Dictionaries and Lambdas.py
+++ Lessons 02 (2024)/Lesson 03 - Dictionaries and Lambdas.py
@@ -31,16 +31,16 @@ b = 4
 df = pd.DataFrame({'column A': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 'column B': [0, 0, 2, 2, 0, 0, 4, 4, 0, 0]})
 
 for key in ["add", "mul", "pow", "div"]:
-    print(f'The {key} function off {a} and {b} is {dict_math_functions[key](a, b)}')
+    print(f'The {key} function of {a} and {b} is {dict_math_functions[key](a, b)}')
 print()
-print(f'The addition off {a} and {b} is {addition(a, b)}')
-print(f'The multiply off {a} and {b} is {multiply(a, b)}')
-print(f'The power off {a} and {b} is {power(a, b)}')
-print(f'The division off {a} and {b} is {division(a, b)}')
+print(f'The addition of {a} and {b} is {addition(a, b)}')
+print(f'The multiply of {a} and {b} is {multiply(a, b)}')
+print(f'The power of {a} and {b} is {power(a, b)}')
+print(f'The division of {a} and {b} is {division(a, b)}')
 print()
 
 print(df)
 print(f'new df is {df.map(olala)}')
 print(f'new df is {df.map(lambda s: str(s) + '-olala')}')
 for key in ["add", "mul", "pow", "div"]:
-    print(f'The {key} function off the df is {df.map(lambda n: dict_math_functions[key](n, 2))}')
+    print(f'The {key} function of the df is {df.map(lambda n: dict_math_functions[key](n, 2))}')
