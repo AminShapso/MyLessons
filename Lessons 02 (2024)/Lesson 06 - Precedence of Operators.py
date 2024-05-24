6d1df6eb45c6143e4a61025a71c3dde57ae1b925
Amin-Sha <157518686+Amin-Sha@users.noreply.github.com>
2024-05-24 15:47:58 +0300

    Create Lesson 06 - Precedence of Operators.py

---
+++ Lessons 02 (2024)/Lesson 06 - Precedence of Operators.py
@@ -0,0 +1,27 @@
+# Taken from:
+# https://pythonflood.com/python-operator-precedence-simplifying-complex-expressions-22eb46b334
+
+# The order for Logical operations:
+# 1. NOT → righ to left
+# 2. AND → left to right
+# 3. OR → left to right
+
+def this_is_just_a_number(number):
+    print("this_is_just_a_number()")
+    return number
+
+print(f"{True and False or False = }")
+print(f"{True or False and False = }")
+print(f"{(True or False) and False = }")
+print()
+
+print(f"{not False and False = }")
+print(f"{not (False and False) = }")
+print()
+
+if this_is_just_a_number(1):
+    print("yes")
+
+a = 0 + 0
+if a + 1 == 1:
+    print("yes")
